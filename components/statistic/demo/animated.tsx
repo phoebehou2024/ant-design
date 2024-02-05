cf1c4e44511873a1e61ee7c4149ca84e305138d9
phoebehou2024 <158983553+phoebehou2024@users.noreply.github.com>
2024-02-05 11:26:40 +0800

    Update animated.tsx
    Signed-off-by: phoebehou2024 <158983553+phoebehou2024@users.noreply.github.com>

--- components/statistic/demo/animated.tsx
+++ components/statistic/demo/animated.tsx
@@ -7,10 +7,10 @@ const formatter = (value: number) => <CountUp end={value} separator="," />;
 const App: React.FC = () => (
   <Row gutter={16}>
     <Col span={12}>
-      <Statistic title="Active Users" value={112893} formatter={formatter} />
+      <Statistic title="Active Users" value={882900000} formatter={formatter} />
     </Col>
     <Col span={12}>
-      <Statistic title="Account Balance (CNY)" value={112893} precision={2} formatter={formatter} />
+      <Statistic title="Account Balance (CNY)" value={882900000} precision={2} formatter={formatter} />
     </Col>
   </Row>
 );
